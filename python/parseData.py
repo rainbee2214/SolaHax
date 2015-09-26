38caf704b9a151ce1c71ea022f3a05eee489f0cf
Sarah <rainbee2214@gmail.com>
2015-09-26 12:13:08 -0300

    Added git ignore.

--- python/parseData.py
+++ python/parseData.py
@@ -1,38 +1,53 @@
 import csv
 
-f = csv.reader(open("home.csv"))
-data = []
 
-ids = []
-postalCodes = []
-conventionalSystems = []
-solarSystems = []
-roofPitches = []
-azimuths = []
-installationTypes = []
-ageOfHomes = []
-sizeOfHomes = []
-waterConsumption = []
-electricityConsumption = []
 
-for f1 in f:
-	data.append(f1)
+def parse_HouseData():
+	f = csv.reader(open("home.csv"))
+	data = []
 
-for d in data:
-	ids.append(d[0])
-	postalCodes.append(d[3]) 
-	conventionalSystems.append(d[4])
-	solarSystems.append(d[5])
-	roofPitches.append(d[6])
-	azimuths.append(d[7])
-	installationTypes.append(d[8])
-	ageOfHomes.append(d[9])
-	sizeOfHomes.append(d[10])
-	waterConsumption.append(d[11])
-	electricityConsumption.append(d[12])
+	ids = []
+	postalCodes = []
+	conventionalSystems = []
+	solarSystems = []
+	roofPitches = []
+	azimuths = []
+	installationTypes = []
+	ageOfHomes = []
+	sizeOfHomes = []
+	waterConsumption = []
+	electricityConsumption = []
 
-postalCodesSet = set(postalCodes)
-for p in postalCodesSet:
-	print (p)
-print (len(postalCodesSet))
+	for f1 in f:
+		data.append(f1)
 
+	for d in data:
+		ids.append(d[2])
+		postalCodes.append(d[3]) 
+		conventionalSystems.append(d[4])
+		solarSystems.append(d[5])
+		roofPitches.append(d[6])
+		azimuths.append(d[7])
+		installationTypes.append(d[8])
+		ageOfHomes.append(d[9])
+		sizeOfHomes.append(d[10])
+		waterConsumption.append(d[11])
+		electricityConsumption.append(d[12])
+
+	idsSet = set(ids)
+	for p in ids:
+		print (p)
+	print(len(ids) == len(idsSet))
+
+
+def parse_ReadingData():
+	f = csv.reader(open("reading.csv"))
+	data = []
+
+	for f1 in f:
+		data.append(f1)
+
+	for d in data:
+
+
+	
