6f661b53dbd340a4f9283c6dcce6dc9d3211ef0b
Sarah <rainbee2214@gmail.com>
2015-09-27 01:54:21 -0300

    Working on indivual files now.

--- python/parseCondensedReadingData.py
+++ python/parseCondensedReadingData.py
@@ -3,18 +3,12 @@ import csv
 inFile = open("data/readingCondensed.csv")
 inReader = csv.reader(inFile)
 
-lastId = 0
-houseCount = 0
-outFile = open("data/houses/house"+str(houseCount)+".csv", 'w')
-outWriter = csv.writer(outFile)
+lastId = "0"
+
 for line in inReader:
-	if (line[1] != lastId):
-		outFile.close()
-		houseCount += 1
-		outFile = open("data/houses/house"+str(houseCount)+".csv", 'w')
-		outWriter = csv.writer(outFile)
+	if (lastId == line[1]):
 		lastId = line[1]
-	outWriter.writerow(line)
+		print(lastId)
 
 inFile.close()
 
