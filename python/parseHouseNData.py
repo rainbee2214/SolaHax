2a952d3a579d36ae1e95ff111ab9de111178c9f6
Sarah <rainbee2214@gmail.com>
2015-09-27 12:55:41 -0300

    In case I break something.

--- python/parseHouseNData.py
+++ python/parseHouseNData.py
@@ -22,72 +22,74 @@ for line in inReader:
 
 inFile.close()
 
-n = 2
+for x in range(0, 48):
+	n = x
 
-inFile = open("data/houses/house"+str(n)+".csv")
-inReader = csv.reader(inFile)
+	inFile = open("data/houses/house"+str(n)+".csv")
+	inReader = csv.reader(inFile)
 
-# always add to grandTotal
-grandTotalPowerGenerated = 0
+	# always add to grandTotal
+	grandTotalPowerGenerated = 0
 
-# only add to totalPowerGenerated if kw > 100 [used for the average]
-totalPowerGeneratedPerDay = 0
-totalMinutesWithPowerPerDay = 1
-welId = ""
-conventionalSystem = ""
-Date = namedtuple('Date', ['year', 'month', 'day'])
-Day = namedtuple('Day', ['averagePowerGenerated', 'totalPowerGenerated', 'date'])
-days = []
+	# only add to totalPowerGenerated if kw > 100 [used for the average]
+	totalPowerGeneratedPerDay = 0
+	totalMinutesWithPowerPerDay = 1
+	welId = ""
+	conventionalSystem = ""
+	Date = namedtuple('Date', ['year', 'month', 'day'])
+	Day = namedtuple('Day', ['averagePowerGenerated', 'totalPowerGenerated', 'date'])
+	days = []
 
-lastDay = ""
-i = 0
-for line in inReader:
-	welId = line[1]
-# append to the total power generated, if kW > 100 add to
-	power = float(line[-1])*float(homes[line[1]])
-	grandTotalPowerGenerated += power
+	lastDay = ""
+	i = 0
+	for line in inReader:
+		welId = line[1]
+	# append to the total power generated, if kW > 100 add to
+		power = float(line[-1])*float(homes[line[1]])
+		grandTotalPowerGenerated += power
 
-	if power > 100:
-		totalPowerGeneratedPerDay += power
-		totalMinutesWithPowerPerDay += 1
+		if power > 100:
+			totalPowerGeneratedPerDay += power
+			totalMinutesWithPowerPerDay += 1
 
-	if line[2][:10] != lastDay:
-		lastDay = line[2][:10]
-		print(line[2][:10])
-		days.append(Day(totalPowerGeneratedPerDay/totalMinutesWithPowerPerDay, grandTotalPowerGenerated, Date(line[2][:4], line[2][5:7], line[2][8:10])))
-		grandTotalPowerGenerated = 0
-		totalPowerGeneratedPerDay = 0
-		totalMinutesWithPowerPerDay = 1
+		if line[2][:10] != lastDay:
+			lastDay = line[2][:10]
+			# print(line[2][:10])
+			days.append(Day(totalPowerGeneratedPerDay/totalMinutesWithPowerPerDay, grandTotalPowerGenerated, Date(line[2][:4], line[2][5:7], line[2][8:10])))
+			grandTotalPowerGenerated = 0
+			totalPowerGeneratedPerDay = 0
+			totalMinutesWithPowerPerDay = 1
 
-	# print("Time:",line[2], "Solar Power:", line[-1], "Solar System:", homes[line[1]], "Power generated:", power)
+		# print("Time:",line[2], "Solar Power:", line[-1], "Solar System:", homes[line[1]], "Power generated:", power)
 
-overallPowerGenerated = 0
-overallDayAverages = 0
-averagePerDayGenerated = 0
-daysGenerated = 0
-fuelCostOil = 0.2
-fuelCostElectricity = 0.17
-conventionalSystem = homesConventionalType[welId]
-
-
-for day in days:
-	daysGenerated += 1
-	overallPowerGenerated += day[1]
-	overallDayAverages += day[0]
-	print("Date", day[2][0], day[2][1], day[2][2],"Average Power:", day[0], "Total Power:", day[1], "Money Saved:", fuelCostOil*day[1])
-
-# Fuel cost (oil) = $0.20 per kWh of oil
-# Fuel cost (electricity) = $0.17 per kWh of electricity
+	overallPowerGenerated = 0
+	overallDayAverages = 0
+	averagePerDayGenerated = 0
+	daysGenerated = 0
+	fuelCostOil = 0.2
+	fuelCostElectricity = 0.17
+	conventionalSystem = homesConventionalType[welId]
 
 
-moneySavings = 0.0
+	for day in days:
+		daysGenerated += 1
+		overallPowerGenerated += day[1]
+		overallDayAverages += day[0]
+		# print("Date", day[2][0], day[2][1], day[2][2],"Average Power:", day[0], "Total Power:", day[1], "Money Saved:", fuelCostOil*day[1])
 
-if conventionalSystem == 'Oil':
-	moneySavings = fuelCostOil*overallPowerGenerated
-else:
-	moneySavings = fuelCostElectricity*overallPowerGenerated
-print("Total energy in kWh", overallPowerGenerated, "$", moneySavings)
+	# print ("Average power generated per day", overallPowerGenerated/daysGenerated)
+	# Fuel cost (oil) = $0.20 per kWh of oil
+	# Fuel cost (electricity) = $0.17 per kWh of electricity
 
-print("House:", welId, "Days in use:", len(days), "Average power per day generated", overallDayAverages/daysGenerated, 
-	"Total power generated over time", overallPowerGenerated, "Conventional System Used:", conventionalSystem, "Money saved overall", moneySavings)
-inFile.close()
+
+	moneySavings = 0.0
+
+	if conventionalSystem == 'Oil':
+		moneySavings = fuelCostOil*overallPowerGenerated
+	else:
+		moneySavings = fuelCostElectricity*overallPowerGenerated
+	# print("Total energy in kWh", overallPowerGenerated, "$", moneySavings)
+
+	print("House:", welId, "Days in use:", len(days),"Average power generated per day", overallPowerGenerated/daysGenerated, "Average power per day generated per minute", overallDayAverages/daysGenerated, 
+		"Total power generated over time", overallPowerGenerated, "Conventional System Used:", conventionalSystem)
+	inFile.close()
