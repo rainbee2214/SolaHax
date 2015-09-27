aea94a920201f2bcad28c64eb646bed7c6c119a0
Sarah <rainbee2214@gmail.com>
2015-09-27 01:53:03 -0300

    This killed it. But it worked.

--- python/parseCondensedReadingData.py
+++ python/parseCondensedReadingData.py
@@ -3,8 +3,18 @@ import csv
 inFile = open("data/readingCondensed.csv")
 inReader = csv.reader(inFile)
 
+lastId = 0
+houseCount = 0
+outFile = open("data/houses/house"+str(houseCount)+".csv", 'w')
+outWriter = csv.writer(outFile)
 for line in inReader:
-
+	if (line[1] != lastId):
+		outFile.close()
+		houseCount += 1
+		outFile = open("data/houses/house"+str(houseCount)+".csv", 'w')
+		outWriter = csv.writer(outFile)
+		lastId = line[1]
+	outWriter.writerow(line)
 
 inFile.close()
 
